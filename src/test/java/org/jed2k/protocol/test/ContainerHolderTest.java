06e6d6da93f8e23513917ad0f58b76b7c5033fb3
a-pavlov <dkfsoft@gmail.com>
2016-07-01 23:09:31 +0500

    Test Container class instead of ContainerHolder

--- src/test/java/org/jed2k/protocol/test/ContainerHolderTest.java
+++ src/test/java/org/jed2k/protocol/test/ContainerHolderTest.java
@@ -4,38 +4,50 @@ import static junit.framework.Assert.assertEquals;
 
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
+import java.util.Iterator;
 import java.util.LinkedList;
 
+import org.jed2k.protocol.UInt16;
 import org.junit.Test;
 import org.jed2k.exception.JED2KException;
 import org.jed2k.protocol.UInt8;
-import org.jed2k.protocol.ContainerHolder;
+import org.jed2k.protocol.Container;
 import org.jed2k.protocol.NetworkIdentifier;
 
-import static org.jed2k.protocol.Unsigned.uint8;
+import static junit.framework.Assert.assertFalse;
+import static junit.framework.Assert.assertTrue;
 
 public class ContainerHolderTest {
     
     @Test
-    public void testHolderSerialization() throws JED2KException {
+    public void testContainerSerialization() throws JED2KException {
         byte source[] = {
                 (byte)0x02, // size
                 (byte)0x01, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x05, (byte)0x00,   // net identifier 1
                 (byte)0x02, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x06, (byte)0x00};  // net identifier 2
-         
-        LinkedList<NetworkIdentifier> nets = new LinkedList<NetworkIdentifier>();
-        ContainerHolder<UInt8, NetworkIdentifier> cni = ContainerHolder.make8(nets, NetworkIdentifier.class);
-        //assertEquals(0, cni.)
-        //assertTrue(cni != null);
+
+        Container<UInt8, NetworkIdentifier> cni = Container.makeByte(NetworkIdentifier.class);
         ByteBuffer nb = ByteBuffer.wrap(source);
         nb.order(ByteOrder.LITTLE_ENDIAN);
         cni.get(nb);
         assertEquals(2, cni.size());
-        
-        assertEquals(1, nets.get(0).ip);
-        assertEquals(2, nets.get(1).ip);
-        
-        assertEquals(5, nets.get(0).port);
-        assertEquals(6, nets.get(1).port);
+
+        Iterator<NetworkIdentifier> itr = cni.iterator();
+        assertTrue(itr.hasNext());
+        assertEquals(new NetworkIdentifier(1, (short)5), itr.next());
+        assertTrue(itr.hasNext());
+        assertEquals(new NetworkIdentifier(2, (short)6), itr.next());
+        assertFalse(itr.hasNext());
+    }
+
+    @Test
+    public void testContainerInvariants() {
+        Container<UInt16, UInt16> c1 = Container.makeShort(UInt16.class);
+        Iterator<UInt16> itr = c1.iterator();
+        assertTrue(itr != null);
+        assertFalse(itr.hasNext());
+        c1.add(new UInt16(10));
+        assertTrue(c1.iterator().hasNext());
+        assertEquals(10, c1.iterator().next().intValue());
     }
 }
