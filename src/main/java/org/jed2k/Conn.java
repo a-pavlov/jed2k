08cfe53ecd8f7fe651d8d72482cda4c9a548be0d
a-pavlov <dkfsoft@gmail.com>
2016-07-25 15:46:26 +0500

    Add experemental alerts reader in dedicated thread, not synchonized output to system out here

--- src/main/java/org/jed2k/Conn.java
+++ src/main/java/org/jed2k/Conn.java
@@ -3,20 +3,72 @@ package org.jed2k;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.*;
+import java.util.logging.Handler;
 import java.util.logging.Logger;
 import java.net.InetSocketAddress;
 
+import org.jed2k.alert.Alert;
+import org.jed2k.alert.SearchResultAlert;
+import org.jed2k.alert.ServerMessageAlert;
+import org.jed2k.alert.ServerStatusAlert;
 import org.jed2k.exception.JED2KException;
 import org.jed2k.protocol.NetworkIdentifier;
+import org.jed2k.protocol.server.SharedFileEntry;
 import org.jed2k.protocol.server.search.SearchRequest;
+import org.jed2k.protocol.server.search.SearchResult;
 
 public class Conn {
     private static Logger log = Logger.getLogger(Conn.class.getName());
     
     public static void main(String[] args) throws IOException {
+        Logger logger = Logger.getLogger("");
+        logger.setUseParentHandlers(false);
+        //Handler[] handlers = logger.getHandlers();
+        //for(Handler handler : handlers) {
+        //    logger.removeHandler(handler);
+        //}
+
         System.out.println("Conn started");
-        Session s = new Session();
+        final Session s = new Session();
         s.start();
+
+        ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(1);
+
+        ScheduledFuture scheduledFuture =
+            scheduledExecutorService.scheduleAtFixedRate(new Runnable() {
+                    @Override
+                    public void run() {
+                    Alert a = s.popAlert();
+                    while(a != null) {
+                        if (a instanceof SearchResultAlert) {
+                            SearchResult sr = ((SearchResultAlert)a).results;
+                            for(SharedFileEntry entry: sr.files) {
+                                System.out.println(entry.toString());
+                            }
+
+                            System.out.println("More results: " + (sr.hasMoreResults()?"yes":"no"));
+                        }
+                        else if (a instanceof ServerMessageAlert) {
+                            System.out.println("Server message: " + ((ServerMessageAlert)a).msg);
+                        }
+                        else if (a instanceof ServerStatusAlert) {
+                            ServerStatusAlert ssa = (ServerStatusAlert)a;
+                            System.out.println("Files count = " + ssa.filesCount + " users count = " + ssa.usersCount);
+                        }
+                        else {
+                            System.out.println("Unknown alert received: " + a.toString());
+                        }
+
+                        a = s.popAlert();
+                    }
+                }
+            },
+        1, 1,
+        TimeUnit.SECONDS);
+
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         String command;
         
@@ -40,7 +92,7 @@ public class Conn {
             }
             if (parts[0].compareTo("connect") == 0 && parts.length == 3) {
                 s.connectoTo(new InetSocketAddress(parts[1], (short)Integer.parseInt(parts[2])));
-            } 
+            }
             else if (parts[0].compareTo("search") == 0 && parts.length > 1) {
                 String searchExpression = command.substring("search".length());
                 log.info("search expressionr:" + searchExpression);
@@ -54,8 +106,9 @@ public class Conn {
                 s.connectToPeer(new NetworkIdentifier(Integer.parseInt(parts[1]), (short)Integer.parseInt(parts[2])));
             }
             
-        }       
-        
-        log.info("Conn finished");        
+        }
+
+        scheduledExecutorService.shutdown();
+        log.info("Conn finished");
     }
 }
