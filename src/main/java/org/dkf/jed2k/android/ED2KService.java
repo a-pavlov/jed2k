bfe6d7caeef0fa1d754106a0df11de846cb34c77
a-pavlov <dkfsoft@gmail.com>
2016-08-31 23:42:28 +0500

    Prepare empty android service

---
+++ src/main/java/org/dkf/jed2k/android/ED2KService.java
@@ -0,0 +1,13 @@
+package org.dkf.jed2k.android;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class ED2KService extends Service {
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
