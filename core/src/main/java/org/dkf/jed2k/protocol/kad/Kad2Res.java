59a8ff1cb44bae7454ddc6aa380005dac5195363
a-pavlov <dkfsoft@gmail.com>
2016-11-21 21:50:44 +0500

    Add base req/res kademlia2 packets

---
+++ core/src/main/java/org/dkf/jed2k/protocol/kad/Kad2Res.java
@@ -0,0 +1,37 @@
+package org.dkf.jed2k.protocol.kad;
+
+import lombok.Getter;
+import org.dkf.jed2k.exception.JED2KException;
+import org.dkf.jed2k.protocol.Container;
+import org.dkf.jed2k.protocol.UInt8;
+
+import java.nio.ByteBuffer;
+
+/**
+ * Created by inkpot on 21.11.2016.
+ */
+@Getter
+public class Kad2Res extends Transaction {
+    private KadId target = new KadId();
+    private Container<UInt8, KadEntry> results = Container.makeByte(KadEntry.class);
+
+    @Override
+    public byte getTransactionId() {
+        return Transaction.REQ_RES;
+    }
+
+    @Override
+    public ByteBuffer get(ByteBuffer src) throws JED2KException {
+        return results.get(target.get(src));
+    }
+
+    @Override
+    public ByteBuffer put(ByteBuffer dst) throws JED2KException {
+        return results.put(target.put(dst));
+    }
+
+    @Override
+    public int bytesCount() {
+        return target.bytesCount() + results.bytesCount();
+    }
+}
--- core/src/main/java/org/dkf/jed2k/protocol/kad/Transaction.java
+++ core/src/main/java/org/dkf/jed2k/protocol/kad/Transaction.java
@@ -11,6 +11,7 @@ public abstract class Transaction implements Serializable {
     public static final byte BOOTSTRAP = 0x01;
     public static final byte PING_PONG = 0x02;
     public static final byte HELLO = 0x03;
+    public static final byte REQ_RES = 0x04;
 
     public abstract byte getTransactionId();
 }
