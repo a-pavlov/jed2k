d6a3b79ce05371c3ee70b4e45829e3ea9d5e19ab
a-pavlov <dkfsoft@gmail.com>
2016-12-14 22:00:48 +0500

    Move Refresh algorithm to Kad2Ping packet

--- core/src/main/java/org/dkf/jed2k/kad/traversal/algorithm/Refresh.java
+++ core/src/main/java/org/dkf/jed2k/kad/traversal/algorithm/Refresh.java
@@ -1,19 +1,33 @@
 package org.dkf.jed2k.kad.traversal.algorithm;
 
+import lombok.extern.slf4j.Slf4j;
+import org.dkf.jed2k.Utils;
+import org.dkf.jed2k.kad.NodeEntry;
 import org.dkf.jed2k.kad.NodeImpl;
 import org.dkf.jed2k.kad.traversal.observer.Observer;
 import org.dkf.jed2k.kad.traversal.observer.RefreshObserver;
 import org.dkf.jed2k.protocol.Endpoint;
-import org.dkf.jed2k.protocol.kad.Kad2HelloReq;
+import org.dkf.jed2k.protocol.kad.Kad2Ping;
 import org.dkf.jed2k.protocol.kad.KadId;
-import org.dkf.jed2k.protocol.kad.PacketCombiner;
+import org.dkf.jed2k.protocol.kad.ObserverCompareRef;
+
+import java.util.List;
 
 /**
  * Created by inkpot on 03.12.2016.
  */
+@Slf4j
 public class Refresh extends Traversal {
     public Refresh(NodeImpl ni, KadId t) {
         super(ni, t);
+
+        List<NodeEntry> nodes = ni.getTable().findNode(t, false, 50);
+        for(final NodeEntry e: nodes) {
+            results.add(newObserver(e.getEndpoint(), e.getId()));
+        }
+        boolean sorted = Utils.isSorted(results, new ObserverCompareRef(t));
+        assert sorted;
+        log.debug("[refresh] initial size is {}", results.size());
     }
 
     @Override
@@ -23,11 +37,7 @@ public class Refresh extends Traversal {
 
     @Override
     public boolean invoke(final Observer o) {
-        Kad2HelloReq hello = new Kad2HelloReq();
-        hello.setKid(nodeImpl.getSelf());
-        hello.getPortTcp().assign(nodeImpl.getPort());
-        hello.getVersion().assign(PacketCombiner.KADEMLIA_VERSION5_48a);
-        return nodeImpl.invoke(hello, o.getEndpoint(), o);
+        return nodeImpl.invoke(new Kad2Ping(), o.getEndpoint(), o);
     }
 
     @Override
