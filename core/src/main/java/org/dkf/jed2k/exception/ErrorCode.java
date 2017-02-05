10c81d1d122d1f135c121da816762b3155fed886
a-pavlov <dkfsoft@gmail.com>
2017-02-06 00:22:45 +0500

    Merge error codes from kad

--- core/src/main/java/org/dkf/jed2k/exception/ErrorCode.java
+++ core/src/main/java/org/dkf/jed2k/exception/ErrorCode.java
@@ -18,8 +18,6 @@ public enum ErrorCode implements BaseErrorCode {
     TAG_TO_LONG_INVALID(13, "Tag to long conversion error"),
     TAG_TO_FLOAT_INVALID(14, "Tag to float conversion error"),
     TAG_TO_HASH_INVALID(15, "Tag to hash conversion error"),
-    TAG_TO_BLOB_INVALID(50, "Tag to blob conversion error"),
-    TAG_TO_BSOB_INVALID(51, "Tag to bsob coversion error"),
     TAG_FROM_STRING_INVALID_CP(16, "Tag from string creation error invalid code page"),
     GENERIC_INSTANTIATION_ERROR(17, "Generic instantiation error"),
     GENERIC_ILLEGAL_ACCESS(18, "Generic illegal access"),
@@ -57,8 +55,9 @@ public enum ErrorCode implements BaseErrorCode {
     URI_SYNTAX_ERROR(48, "URI has incorrect syntax"),
     NUMBER_FORMAT_ERROR(49, "Parse number exception"),
     UNKNOWN_LINK_TYPE(50, "Emule link has unrecognized type"),
-    FAIL(51, "Fail");
-    PACKET_HEADER_UNDEFINED(49, "Packet header contains wrong bytes or undefined"),
+    PACKET_HEADER_UNDEFINED(51, "Packet header contains wrong bytes or undefined"),
+    TAG_TO_BLOB_INVALID(52, "Tag to blob conversion error"),
+    TAG_TO_BSOB_INVALID(53, "Tag to bsob coversion error"),
     FAIL(60, "Fail");
 
     private final int code;
